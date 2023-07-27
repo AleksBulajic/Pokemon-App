872eaea66a11b0d2eef61c95d1e551361a300d10
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-27 12:16:36 -0400

    pagination started

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./component/PokemonList";
 import axios from "axios";
+import Pagination from "./component/Pagination";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
-  const [nextPageUrl, setNextPageUrl] = useState()
-  const [previusPageUrl, setPreviusPageUrl] = useState()
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previusPageUrl, setPreviusPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(currentPageUrl).then((res) => {
-      setNextPageUrl(res.data.next)
-      setPreviusPageUrl(res.data.previous)
-      setPokemon(res.data.results.map((p) => p.name));
-    });
+    setLoading(true);
+    let cancel;
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(currentPageUrl, {
+          cancelToken: new axios.CancelToken((c) => (cancel = c)),
+        });
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPreviusPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          // Request canceled
+        } else {
+          // Handle error
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => cancel();
   }, [currentPageUrl]);
 
-  return <PokemonList pokemon={pokemon} />;
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function gotoPreviusPage() {
+    setCurrentPageUrl(previusPageUrl);
+  }
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination gotoNextPage={gotoNextPage} gotoPreviusPage={gotoPreviusPage} />
+    </>
+  );
 }
 
 export default App;
--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function PokemonList({pokemon}) {
   
+    const pokemonItam = pokemon.map(p => <div key={p}>{p}</div>)
+
+    
   return ( 
     <div>
-    {pokemon.map(p => (
-        <div key={p} >{p}</div>
-    ))}
+     <div>{pokemonItam}</div>
     </div>
   )
 }
