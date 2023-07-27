166a0413315b79b6fecd78852a20a9e428940fc0
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-27 11:54:08 -0400

    structure set/axios fetxhing data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Description from './components/Description';
-import PokemonThumbnail from './components/PokemonThumbnail';
-
-
+import React, { useState, useEffect } from "react";
+import PokemonList from "./component/PokemonList";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [previusPageUrl, setPreviusPageUrl] = useState()
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then((res) => {
+      setNextPageUrl(res.data.next)
+      setPreviusPageUrl(res.data.previous)
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, [currentPageUrl]);
+
+  return <PokemonList pokemon={pokemon} />;
 }
 
 export default App;
--- src/components/PokemonThumbnail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PokemonThumbnail() {
-  return (
-    <div>PokemonThumbnail</div>
-  )
-}
-
-export default PokemonThumbnail
